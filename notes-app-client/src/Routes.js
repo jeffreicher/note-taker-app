c6f8f8c980dbcb11335df485c69395863776deed
Jeffrei Cher <jeff@baselinesoftware.io>
2018-09-14 14:32:44 -0700

    added new note container and max file size limit

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -5,12 +5,19 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoutes";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNotes";
 
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute
+      path="/notes/new"
+      exact
+      component={NewNote}
+      props={childProps}
+    />
     <Route component={NotFound} />
   </Switch>
 );
